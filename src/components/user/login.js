120c33172dd1208aa20d353ec6dce43acb6ea542
huoban-lzh <lizhi@huoban.com>
2019-07-30 15:32:57 +0800

    user dom

--- src/components/user/login.js
+++ src/components/user/login.js
@@ -9,13 +9,27 @@ class Login extends Component {
     }
   }
 
+  handleUsernameChange = (e) => {
+    this.setState({username: e.target.value})
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({password: e.target.value})
+  }
+
+  handleLogin = () => {
+    const {username, password} = this.state
+    console.log({username, password})
+  }
+
   render() {
+    const {username, password} = this.state
     return (
       <div>
         <h3>登录</h3>
-        <p>用户名：</p><input type="text" />
-        <p>密码：</p><input type="password" />
-        <input type="button" value='登录' />
+        <p>用户名：</p><input type="text" value={username} onChange={this.handleUsernameChange} />
+        <p>密码：</p><input type="password"  value={password} onChange={this.handlePasswordChange} />
+        <input type="button" value='登录' onClick={this.handleLogin} />
         <input type="button" value='立即注册' />
       </div>
     )
--- src/components/user/register.js
+++ src/components/user/register.js
@@ -1,18 +1,46 @@
-import React, { Components } from 'react'
+import React, { Component } from 'react'
 
-class Register extends Components {
+class Register extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
+
+  handleUsernameChange = (e) => {
+    this.setState({username: e.target.value})
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({password: e.target.value})
+  }
+
+  handleConfirmPasswordChange = (e) => {
+    this.setState({confirmPassword: e.target.value})
+  }
+
+  handleRegister = () => {
+    const {username, password, confirmPassword} = this.state
+    console.log({username, password, confirmPassword})
+  }
+
   render() {
+    const {username, password, confirmPassword} = this.state
+
     return (
       <div>
         <h3>注册</h3>
         <p>用户名：</p>
-        <input type="text" />
-        <p></p>
-        <input type="text" />
-        <p></p>
-        <input type="password"/>
-        <input type="button"/>
-        <input type="button"/>
+        <input type="text" value={username} onChange={this.handleUsernameChange} />
+        <p>密码：</p>
+        <input type="password" value={password} onChange={this.handlePasswordChange} />
+        <p>确认密码：</p>
+        <input type="password" value={confirmPassword} onChange={this.handleConfirmPasswordChange} />
+        <input type="button" value='注册' onClick={this.handleRegister} />
+        <input type="button" value='立即登录' />
       </div>
     )
   }
