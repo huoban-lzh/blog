bd949cb9aa2596bff6d0aa03c259c2b0c669f0cd
huoban-lzh <lizhi@huoban.com>
2019-07-30 15:31:40 +0800

    init

---
+++ src/pages/register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Register from '../components/user/register'
+
+class RegisterPage extends Component {
+  render() {
+    return (
+      <div>
+        <Register />
+      </div>
+    )
+  }
+}
+
+export default RegisterPage
