36947b25bc2931ad8604cf2c39e2495e8708635f
huoban-lzh <lizhi@huoban.com>
2019-07-31 15:38:13 +0800

    init

---
+++ src/pages/add.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Add from '../components/blog/add'
+
+class AddPage extends Component {
+  render() {
+    return (
+      <div>
+        <Add />
+      </div>
+    )
+  }
+}
+
+export default AddPage
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Index from '../components/blog/index'
+
+class IndexPage extends Component {
+  render() {
+    return (
+      <div>
+        <Index />
+      </div>
+    )
+  }
+}
+
+export default IndexPage
