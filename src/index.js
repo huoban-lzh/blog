07de9b62ebc01bf5e675f21c7a0b81183b845ee7
huoban-lzh <lizhi@huoban.com>
2019-08-05 11:59:57 +0800

    add article page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import './index.css';
+import './index.css'
 import './App.css'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
 
 import Index from './pages/index'
 import Add from './pages/add'
 import Login from './pages/login'
 import Register from './pages/register'
+import Article from './pages/article'
 
 ReactDOM.render(
   <Router history={browserHistory}>
@@ -16,7 +17,8 @@ ReactDOM.render(
     <Route path='/blog/add' components={Add} />
     <Route path='/user/login' components={Login} />
     <Route path='/user/register' components={Register} />
+    <Route path='/blog/article' components={Article} />
   </Router>,
   document.getElementById('root')
-  );
+)
 
